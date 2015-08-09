3a51af5e6420ac6499b3e5b256c8a2a253a72119
rzs2009 <rzs2009@yahoo.com>
2015-08-09 19:08:40 +0000

    UserEngine

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :users
+  
+  match '/signup',  to: 'users#new',            via: 'get'
+  
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  match '/logout', to: 'sessions#destroy',     via: 'delete'
+  
+  root to: 'static_pages#home'
+end
